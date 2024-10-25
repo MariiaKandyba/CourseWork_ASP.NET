649646490fa55f76f903a97009a4dc83b3a75844
Admin <Admin@DESKTOP-HEOO8JS>
2024-10-25 15:17:24 +0300

    roles added, user model changed

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -3,8 +3,13 @@
     public class User
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
         public string Email { get; set; }
         public string PasswordHash { get; set; }
+        public bool IsEmailVerified { get; set; } 
+        public ICollection<Role> Roles{ get; set; } 
     }
+
 
 }
