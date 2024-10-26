33296016e64fe09873c45241e7cca62975ce0d09
Admin <Admin@DESKTOP-HEOO8JS>
2024-10-26 22:16:46 +0300

    IsAuthenticated

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.IsAuthenticated = HttpContext.Request.Cookies.ContainsKey("jwtToken");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
