8dde1686941f989105de3eff2929e95b57f4005e
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:31:51 +0200

    OrderDto

---
+++ DTOs/Orders/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Orders
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public List<OrderItemDto> Items { get; set; } = new List<OrderItemDto>();
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; }
+        public AddressDto DeliveryAddress { get; set; } = new AddressDto();
+    }
+}
