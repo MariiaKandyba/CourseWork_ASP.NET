d6f89323e92d3f39b2036f01531cecb0d54c1401
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:58:56 +0200

    order service added

---
+++ Client/Services/Order/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using DTOs.Orders;
+
+namespace Client.Services.Order
+{
+    public interface IOrderService
+    {
+        Task<CreateOrderRequestDto> CreateOrderAsync(CreateOrderRequestDto order);
+        Task<List<OrderDto>> GetOrderByUserAsync(int id);
+
+    }
+}
