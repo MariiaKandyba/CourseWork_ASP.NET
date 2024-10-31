a2151103e6fe2e3630678a7c4f10870f3a67a9e6
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:33:57 +0200

    Address model

---
+++ OrderServiceApi/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using DTOs.Orders;
+using Newtonsoft.Json;
+
+namespace OrderServiceApi.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    }
+}
