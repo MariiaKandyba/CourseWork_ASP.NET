58718c51f713382f26e1d0cba5d7ea692b8f8b4d
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:53:22 +0200

    UserInfoViewComponent

---
+++ Client/Components/UserInfoViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Components
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var userModel = new User
+            {
+                IsAuthenticated = HttpContext.User.Identity.IsAuthenticated
+            };
+
+            if (userModel.IsAuthenticated)
+            {
+                userModel.FirstName = HttpContext.User.FindFirst("firstName")?.Value;
+                userModel.LastName = HttpContext.User.FindFirst("lastName")?.Value;
+            }
+
+            return View(userModel);
+        }
+    }
+}
