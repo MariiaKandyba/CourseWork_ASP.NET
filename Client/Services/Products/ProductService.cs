4c5bde163e712d1ae4f3c3559ab8e740eab1ceae
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:59:12 +0200

    GetProductByIdAsync added

--- Client/Services/Products/ProductService.cs
+++ Client/Services/Products/ProductService.cs
@@ -16,5 +16,17 @@ namespace Client.Services.Products
             var response = await _httpClient.GetAsync("https://localhost:7140/gateway/products");
             return await response.Content.ReadFromJsonAsync<List<ProductDto>>();
         }
+        public async Task<ProductDto> GetProductByIdAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"https://localhost:7140/gateway/products/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<ProductDto>();
+        }
+        public async Task<List<ProductDto>> GetProductsByIdsAsync(List<int> productIds)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7140/gateway/products/batch", productIds);
+            return await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        }
+
     }
 }
