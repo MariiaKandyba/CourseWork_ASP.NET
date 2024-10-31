b21fc58303d948f16df2af40d2b915c6eee89db8
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:57:59 +0200

    viewmodels added to display the cart and orderinfo

---
+++ Client/Models/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DTOs.Orders;
+
+namespace Client.Models
+{
+    public class OrderViewModel
+    {
+        public string FirstName { get; set; } // Ім'я користувача
+        public string LastName { get; set; } // Прізвище користувача
+        public string Email { get; set; } // Електронна адреса користувача
+        public AddressDto DeliveryAddress { get; set; } // Адреса доставки
+        public decimal TotalPrice {get; set; }
+
+        public List<ProductCartViewModel> Items { get; set; } = new(); // Список товарів у кошику
+
+    }
+}
---
+++ Client/Models/UserDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DTOs.Orders;
+
+namespace Client.Models
+{
+    public class UserDetailsViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public AddressDto Address{ get; set; }
+    }
+}
