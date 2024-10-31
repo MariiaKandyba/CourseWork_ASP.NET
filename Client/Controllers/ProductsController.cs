e5e0433ffb7df390daa70cb0c84065de4fa826bb
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-01 00:56:58 +0200

    index to ProductsController added

--- Client/Controllers/ProductsController.cs
+++ Client/Controllers/ProductsController.cs
@@ -4,11 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Client.Controllers
 {
-    public class ProductController : Controller
+    public class ProductsController : Controller
     {
         private readonly ProductService _productService;
 
-        public ProductController(ProductService productService)
+        public ProductsController(ProductService productService)
         {
             _productService = productService;
         }
