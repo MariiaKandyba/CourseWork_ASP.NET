5e07be65240e03c2bff10d2b9db23f4b25df05e8
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-07 14:19:54 +0200

    admin format product service added

---
+++ Client/Areas/Services/StockProductService.cs
@@ -0,0 +1,20 @@
+﻿using DTOs.Admin;
+
+namespace Client.Areas.Services
+{
+    public class StockProductService
+    {
+        private readonly HttpClient _httpClient;
+        public StockProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<FullProductDto>> GetAll()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7140/gateway/products/full");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<FullProductDto>>();
+
+        }
+    }
+}
