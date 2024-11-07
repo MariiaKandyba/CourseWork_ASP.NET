70225e19ca14fa24cd5ead5e5e4f43e1a4883ac9
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-07 14:22:34 +0200

    dtos for admin area added

---
+++ DTOs/Admin/FullOrderDto.cs
@@ -0,0 +1,22 @@
+﻿using DTOs.Orders;
+using DTOs.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Admin
+{
+    public class FullOrderDto
+    {
+        public int Id { get; set; }
+        public List<FullOrderItemDto> Items { get; set; }
+        public UserDto User { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; }
+        public AddressDto DeliveryAddress { get; set; }
+        public decimal TotalPrice { get; set; }
+
+    }
+}
---
+++ DTOs/Admin/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Admin
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<RoleDto> Roles { get; set; }
+
+    }
+}
