27705360be9cc79711385bbcd85749556780f57f
Admin <Admin@DESKTOP-HEOO8JS>
2024-11-07 14:20:07 +0200

    admin userservice added

---
+++ Client/Areas/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using DTOs.Admin;
+using DTOs.Auth;
+using DTOs.Orders;
+
+namespace Client.Areas.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<UserDto>> GetAll()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7140/gateway/users/all");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<UserDto>>();
+
+        }
+
+    }
+}
